8fd91accea350ad012449a08b4c8830e7175d2b8
Daniel <pancudaniel7@gmail.com>
2024-01-04 15:58:43 +0200

    Fixed the ssh_ec2 script

--- utils/ssh_ec2.sh
+++ utils/ssh_ec2.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 # Params:
-# 1 - instance ip
-# 2 - ssh public key file
-# 3 - ec2 machine user
+# 1 - region (default: eu-central-1)
+# 2 - ssh key name (default: jenkins-infra)
+# 3 - ec2 linux user name (default: ubuntu)
 
-# EC2 instance details
-instance_ip=$1
-ssh_key_path="$HOME/.ssh/$2"
-ssh_user="${3:-ubuntu}"
+output=$(aws ec2 describe-instances --region ${1:-eu-central-1} --query "Reservations[*].Instances[*].[InstanceId,PublicIpAddress,Tags[?Key=='Name'].Value | [0]]" --output text)
 
-# SSH connection options
-ssh_options="-i $ssh_key_path -o StrictHostKeyChecking=no"
+echo "AWS Command Output:"
+echo "$output"
+echo ""
 
-# SSH into the EC2 instance
-ssh $ssh_options $ssh_user@$instance_ip
+declare -a instance_ids
+declare -a public_ips
+declare -a names
 
+while read -r instance_id public_ip name; do
+    instance_ids+=("$instance_id")
+    public_ips+=("$public_ip")
+    names+=("$name")
+done <<< "$output"
+
+echo "Select instance you want to conenct by typing the line number,"
+echo "first line starts with number 1:"
+read -r input
+
+ssh -i ~/.ssh/${2:-jenkins-infra} ${3:-ubuntu}@${public_ips[((input-1))]}
