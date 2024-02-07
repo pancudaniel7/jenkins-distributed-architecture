154ccd6e8e8956d1495410bab82c29f047adaab1
Daniel <pancudaniel7@users.noreply.github.com>
2024-02-07 02:21:33 +0200

    Finish ansible provisioning steps

--- infrastructure/modules/security/sg/main.tf
+++ infrastructure/modules/security/sg/main.tf
@@ -9,6 +9,14 @@ resource "aws_security_group" "ec2_security_groups" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    description = "Jenkins port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
